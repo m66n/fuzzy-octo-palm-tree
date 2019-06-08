7e487b2ab04b4d155ad2ade5d1eadefc93c80bf4
Michael Chapman <m5l.c5n@gmail.com>
2019-06-08 11:02:22 -0400

    add shebang, license, and attribution

--- pypwgen/main.py
+++ pypwgen/main.py
@@ -1,3 +1,29 @@
+#!/usr/bin/env python3
+
+# MIT License
+#
+# Copyright (c) 2019 Michael Chapman
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+# based on pwgen (https://sourceforge.net/projects/pwgen/)
+
 import secrets
 import sys
 
@@ -130,7 +156,9 @@ def password(size, options, remove):
 
 
 def main():
-    print(phonemes(20, Option.UPPERS | Option.DIGITS | Option.AMBIGUOUS))
+    options = Option.UPPERS | Option.DIGITS | Option.AMBIGUOUS
+    for i in range(10):
+        print(phonemes(20, options))
     return 0
 
 
